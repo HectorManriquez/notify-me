ba3d1889b886fa9b2dad66826430fc743603dbbc
Keitarokido <hector.j.manriquez@gmail.com>
2016-12-22 00:42:46 -0600

    Adding modal to app so it can be rendered

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Home from './Home/Home';
+import AddNotification from './AddNotification/AddNotification';
 
 class App extends Component {
+    componentDidMount() {
+        $('.modal-trigger').leanModal();
+    }
+
     render() {
         return (
             <div className='container'>
                 <Header/>
                 <Home/>
+                <AddNotification/>
                 <Footer/>
             </div>
         )
